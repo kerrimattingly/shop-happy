a08848244342c6c44d9e8b4ba485a6db36d7166f
Kerri Mattingly <kerrimatt@yahoo.com>
2017-07-10 22:24:49 -0500

    Initial commit

---
+++ src/main/java/com/kerrimattingly/shophappy/ShopHappyApplication.java
@@ -0,0 +1,12 @@
+package com.kerrimattingly.shophappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHappyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kerrimattingly/shophappy/controllers/ItemController.java
@@ -0,0 +1,17 @@
+package com.kerrimattingly.shophappy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * Created by kerrimattingly on 7/9/17.
+ */
+@Controller
+public class ItemController {
+
+    @RequestMapping(value="")
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+}
